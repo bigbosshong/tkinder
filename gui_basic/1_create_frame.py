ab0d2fb86999be9afd25493f61cce82ad1caa798
unknown <bigbosshong1101@gmail.com>
2022-08-17 19:14:55 +0900

    frame, btn

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") #가로 #세로
+# root.geometry("640x480+300+100") #가로 #세로 + x좌표 + y좌표
+
+
+root.resizable(True, False)
+
+root.mainloop()
