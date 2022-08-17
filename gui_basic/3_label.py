df1cc7c69f8fb90d746c7c246c09fade24714496
unknown <bigbosshong1101@gmail.com>
2022-08-17 19:32:00 +0900

    label

---
+++ gui_basic/3_label.py
@@ -0,0 +1,28 @@
+    from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") #가로 #세로
+
+label1 = Label(root, text="Hello!")
+label1.pack()
+
+photo = PhotoImage(file="gui_basic/img.png")
+label2 = Label(root, image=photo)
+label2.pack()
+
+
+def change():
+    label1.config(text="See you again!")
+
+    global photo2
+    photo2 = PhotoImage(file="gui_basic/img2.png")
+
+    label2.config(image=photo2)
+
+btn = Button(root, text="Click", command=change)
+btn.pack()
+
+root.resizable(True, False)
+
+root.mainloop()
